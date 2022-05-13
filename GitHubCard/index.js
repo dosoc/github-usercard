81129c874b90eb0ab76573948b184731cd78ad71
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-05-12 22:46:06 -0500

    finish MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,24 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+function addCards (arr){
+  arr.forEach(user => {
+    axios.get(`https://api.github.com/users/${user}`)
+    .then((res)=>{
+      const entryPoint = document.querySelector(".cards");
+      const card = cardMaker(res.data);
+      entryPoint.appendChild(card);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  });
+}
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,8 +44,56 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["MarcoFalke", "luke-jr", "adamjonas",
+  "laanwj",
+  "sipa",
+  "hebasto",
+  "jnewbery",
+  "fanquake",
+  "achow101",
+  "practicalswift"];
 
+function cardMaker(obj) {
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const username = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const profileLink = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  username.classList.add("username");
+  
+  image.src = obj.avatar_url;
+  name.textContent = obj.name;
+  username.textContent = obj.login;
+  location.textContent = `Location: ${obj.location}`;
+  profile.textContent = `Profile:`
+  profileLink.setAttribute("href", obj.html_url);
+  followers.textContent = `Followers ${obj.followers}`;
+  following.textContent = `Following${obj.following}`;
+  bio.textContent = `Bio:${obj.bio}`;
+
+  card.appendChild(image);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(username);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+  profile.appendChild(profileLink);
+
+  return card;
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -58,3 +122,4 @@ const followersArray = [];
     luishrd
     bigknell
 */
+addCards(followersArray);
